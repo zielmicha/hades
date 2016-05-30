7470176e5fc66691356f60a4b6d19dfa4765c322
Michał Zieliński <michal@zielinscy.org.pl>
2016-05-30 20:14:50 +0200

    x11: allow DRI access for unrestricted profiles

--- hades/common.py
+++ hades/common.py
@@ -1,5 +1,7 @@
 import string
 import os
+import glob
+import stat
 
 def valid_name(s):
     if s.startswith('-') or s.startswith('.'):
@@ -19,3 +21,14 @@ def write_file(path, data):
         data = data.encode('utf8')
     with os.fdopen(fd, 'wb') as f:
         f.write(data)
+
+def add_devices(definition, pattern, uid=0, gid=0, mode=0o660):
+    devices = glob.glob(pattern)
+    for dev in devices:
+        definition['devices']['dev' + dev.replace('/', '--')] = {
+            'type': 'unix-block' if stat.S_ISBLK(os.stat(dev).st_mode) else 'unix-char',
+            'path': dev,
+            'uid': uid,
+            'gid': gid,
+            'mode': oct(mode)[2:]
+        }
--- hades/x11.py
+++ hades/x11.py
@@ -2,7 +2,7 @@ import tempfile
 import os
 import subprocess
 
-from . import core
+from . import core, common
 
 DISPLAY_ID = ':0'
 
@@ -11,7 +11,7 @@ def generate_xauthority(trusted):
         env = os.environ.copy()
         env['XAUTHORITY'] = core.RUN_PATH + '/xauth.' + DISPLAY_ID
         env['DISPLAY'] = DISPLAY_ID
-        subprocess.check_call(['xauth', '-f', f.name, 'generate', DISPLAY_ID, 'MIT-MAGIC-COOKIE-1', 'trusted' if trusted else 'untrusted'], env=env)
+        subprocess.check_call(['xauth', '-f', f.name, 'generate', DISPLAY_ID, 'MIT-MAGIC-COOKIE-1', 'trusted' if trusted else 'untrusted', 'timeout', '31536000'], env=env)
         return open(f.name, 'rb').read()
 
 def setup_shell_env():
@@ -29,6 +29,7 @@ def update_container(self):
     if config.get('x11'):
         if config.get('x11') == 'unrestricted':
             xauthority = open(core.RUN_PATH + '/xauth.' + DISPLAY_ID, 'rb').read()
+            self.run_command(['sh', '-c', 'chown %d:%d /dev/dri/*' % (core.INTERNAL_UID, core.INTERNAL_GID)])
         else:
             xauthority = generate_xauthority(trusted=False)
         self.put_file('/home/%s/.Xauthority' % (self.user.name), xauthority, uid=core.INTERNAL_UID, gid=core.INTERNAL_GID, mode=0o644) # FIXME: permissions
@@ -42,6 +43,12 @@ def update_container_def(self, definition):
             'source': '/tmp/.X11-unix'
         }
         definition['config']['environment.DISPLAY'] = DISPLAY_ID
+
+        if config.get('x11') == 'unrestricted':
+            common.add_devices(definition, '/dev/dri/*',
+                               uid=0,
+                               gid=0,
+                               mode=0o660) # assigning UID is broken in LXD (for users in raw uidmaps)
 
 def add_parsers(addf):
     sub = addf('runx')
